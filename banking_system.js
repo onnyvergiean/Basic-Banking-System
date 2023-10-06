7bac0e96cdcb6b68bed5ad1e3efdd672e19b2279
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2023-10-06 16:00:03 +0700

    Add banking system code and html for challenge 2

---
+++ banking_system.js
@@ -0,0 +1,60 @@
+class BankAccount {
+  constructor(balance) {
+    this.balance = balance;
+  }
+
+  showBalance(balance) {
+    document.getElementById('saldo').textContent = balance;
+  }
+
+  showMessage(message) {
+    document.getElementById('pesan').textContent = message;
+  }
+
+  deposit() {
+    let depositAmount = parseFloat(
+      prompt('Masukkan jumlah saldo yang ingin ditambah: ')
+    );
+
+    if (!isNaN(depositAmount) && depositAmount > 0) {
+      setTimeout(() => {
+        this.balance += depositAmount;
+        this.showMessage(
+          `Saldo berhasil ditambahkan sebesar $${depositAmount}.`
+        );
+        this.showBalance(this.balance);
+      }, 2000);
+      return;
+    }
+    this.showMessage(
+      'Jumlah yang dimasukkan tidak valid atau kurang dari atau sama dengan 0.'
+    );
+  }
+
+  withdraw() {
+    let withdrawAmount = parseFloat(
+      prompt('Masukkan jumlah saldo yang ingin dikurangi: ')
+    );
+
+    if (isNaN(withdrawAmount) || withdrawAmount <= 0) {
+      this.tampilkanPesan(
+        'Jumlah yang dimasukkan tidak valid atau kurang dari atau sama dengan 0.'
+      );
+      return;
+    }
+
+    if (withdrawAmount <= this.balance) {
+      setTimeout(() => {
+        this.balance -= withdrawAmount;
+        this.showBalance(
+          `Saldo berhasil dikurangi sebesar $${withdrawAmount}.`
+        );
+        this.showBalance(this.balance);
+      }, 1000);
+    } else {
+      this.showMessage('Saldo tidak mencukupi.');
+    }
+  }
+}
+
+const bank = new BankAccount(1000000);
