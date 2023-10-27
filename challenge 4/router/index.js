f9756adb6ffb0951ecca04e7bddbd32a10418951
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2023-10-27 21:57:30 +0700

    Move Router into separate files

--- challenge 4/router/index.js
+++ challenge 4/router/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const users = require('./users');
+const accounts = require('./accounts');
+const transactions = require('./transactions');
 
 router.use(users);
+router.use(accounts);
+router.use(transactions);
 
 module.exports = router;
--- challenge 4/router/users.js
+++ challenge 4/router/users.js
@@ -8,15 +8,4 @@ router.delete('/v1/users/:id', controller.users.deleteUser);
 router.post('/v1/users', controller.users.createUser);
 router.put('/v1/users/:id', controller.users.updateUser);
 
-router.post('/v1/accounts/:id', controller.accounts.createAccount);
-router.get('/v1/accounts/:id', controller.accounts.getAccounts);
-router.get('/v1/accounts/:id/:accountId', controller.accounts.getDetailAccount);
-
-router.post('/v1/transactions/', controller.transactions.createTransaction);
-router.get('/v1/transactions/', controller.transactions.getTransactions);
-router.get(
-  '/v1/transactions/:id',
-  controller.transactions.getDetailTransaction
-);
-
 module.exports = router;
