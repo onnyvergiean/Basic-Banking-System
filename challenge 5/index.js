bbb09431b16ec51b7ff8d0b6e9df3ce3405b9715
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2023-11-07 23:25:29 +0700

    Add swagger documentation

--- challenge 5/index.js
+++ challenge 5/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 const routers = require('./router');
@@ -6,6 +7,8 @@ const path = require('path');
 const session = require('express-session');
 const flash = require('express-flash');
 // const passport = require('./utils/passport');
+const swaggerJSON = require('./openapi.json');
+const swaggerUI = require('swagger-ui-express');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,7 +25,11 @@ app.use(flash());
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './app/view'));
-
+app.use(cors());
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJSON));
+app.get('/docs', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+});
 app.use(routers);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
